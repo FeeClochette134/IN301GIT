a40bf84c067243ca6e72f82e5b260f4aa2c276a2
Utilisateur <user@debian.arcahe.ovh>
2019-10-07 13:13:19 +0200

    TD5

---
+++ TD5/IN301_TD5_TRI_INT/tabint.c
@@ -0,0 +1,104 @@
+#include "tabint.h"
+#include "stat.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+// #####
+// 1. Génération, suppression et affichage de tableaux
+// #####
+TABINT gen_alea_tabint (int N, int K) {
+	TABINT T ; 
+	T.T = malloc(N*sizeof(int)) ;
+	T.N = N ;
+	srand(getpid()) ;
+	// Vérification que l'espace a bien pu être alloué
+	if(T.T==NULL) exit(0) ;
+	for(int i=0;i<N;i++) {
+		T.T[i] = rand() % (K+1) ;
+	}
+	return T;
+}
+
+TABINT sup_tabint (TABINT T) {
+	free(T.T) ;
+	return T;
+}
+
+void aff_tabint (TABINT T) {
+	for(int i=0;i<T.N;i++) {
+		printf("case %d : %d\n",i,T.T[i]) ;
+	}
+}
+
+// #####
+// 2. Manipulation des valeurs du tableau
+// #####
+
+// Echange la case i et la case i+1 si la case i est > à la case i+1
+int ech_tabint (TABINT T, int i) {
+	NBCOMP++ ;
+	if(T.T[i]>T.T[i+1]) {
+		int tmp = T.T[i] ;
+		T.T[i] = T.T[i+1] ;
+		T.T[i+1] = tmp ;
+		NBECH++ ;
+		return 1 ;
+	}
+	return 0 ;
+}
+
+//
+int scan_ech_tabint (TABINT T, int fin)  {
+	int a = 0 ;
+	for(int i=0;i<fin;i++) {
+		a = a + ech_tabint(T,i) ;
+	}
+	return a ;
+}
+
+/*void tri_bulle_tabint(TABINT T) {
+	int a = 1 ; int b = 0 ; int pas = 0 ;
+	while(a) {
+		b = T.N - pas ;
+		a = scan_ech_tabint(T,b) ;
+		pas++ ;
+	}
+} */
+
+/*TABINT gen_croissant_tabint() {
+	TABINT T ; int a = 0 ;
+	T.T = malloc(N*sizeof(int)) ;
+	T.N = N ;
+	srand(getpid()) ;
+	// Vérification que l'espace a bien pu être alloué
+	if(T.T==NULL) exit(0) ;
+	for(int i=0;i<N;i++) {
+		a = rand() % (K+1) ;
+	}
+	return T;
+}*/
+
+
+/*int main() {
+	NBECH = 0 ; // Compteur d'échange de contenu
+	NBCOMP = 0 ; // nombre de comparaisons entre cases
+	int N = 10 ; int K = 30 ;
+	TABINT T = gen_alea_tabint(N,K) ;
+	aff_tabint(T) ;
+	printf("trié\n") ;
+	tri_bulle_tabint(T) ;
+	aff_tabint(T) ;
+	
+	printf("nombre de comparaisons de cases : %lld\n",NBCOMP) ;
+	printf("nombre d'echange de contenu : %lld\n",NBECH) ;
+}*/
+
+
+
+
+
+
+
+
+
---
+++ TD5/IN301_TD5_TRI_INT/tri_bulle.c
@@ -0,0 +1,94 @@
+
+#include "tabint.h"
+#include "stat.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void tri_bulle_tabint(TABINT T) {
+	int a = 1 ; int b = 0 ; int pas = 0 ;
+	while(a) {
+		b = T.N - pas ;
+		a = scan_ech_tabint(T,b) ;
+		pas++ ;
+	}
+}
+
+/*TABINT gen_croissant_tabint(int N,int K) {
+	TABINT T ; int a = 0 ;
+	T.T = malloc(N*sizeof(int)) ;
+	T.N = N ;
+	srand(getpid()) ;
+	a = rand() % (K+1) ;
+	while(a>(K-T.N)) a = rand() % (K+1) ;
+	T.T[0] = a ;
+	// Vérification que l'espace a bien pu être alloué
+	if(T.T==NULL) exit(0) ;
+	for(int i=1;i<T.N;i++) {
+		a = rand() % (K+1) ;
+		if(a>=T.T[i-1] && a<(K-T.N+i)) T.T[i] = a ;
+		else i-- ;
+	}
+	return T;
+}*/
+
+struct stat stat_moy (int N, int A) {
+	NBECH = 0 ; // Compteur d'échange de contenu
+	NBCOMP = 0 ; // nombre de comparaisons entre cases
+	int K = 30 ;
+	struct stat stats ;
+	for(int i=0;i<N;i++) {
+		TABINT T = gen_alea_tabint(N,K) ;
+		tri_bulle_tabint(T) ;
+		sup_tabint(T) ;
+	}
+	stats.N = N ;
+	stats.nb_moy_comp = (NBCOMP*1.0)/(A*1.0) ;
+	stats.nb_moy_ech = (NBECH*1.0)/(A*1.0) ;
+	return stats ;
+}
+
+/*void ecrire_dans_test_tri_bulle() {
+	int i;
+	FILE *F ;
+	F = fopen("test_tri_bulle.data","w") ;
+	for(i=10;i<1000;i=i*1.2) {
+		printf("%d\n",i) ;
+		stats = stat_moy(i,10000) ;
+		fprintf(F,"... %d ... %2f ... %2f\n",stats.N,stats.nb_moy_comp,stats.nb_moy_ech) ;
+	}
+	fclose(F) ;
+}*/
+
+
+int main() {
+	
+	NBECH = 0 ; // Compteur d'échange de contenu
+	NBCOMP = 0 ; // nombre de comparaisons entre cases
+	int N = 10 ; int K = 30 ;
+	TABINT T = gen_alea_tabint(N,K) ;
+	aff_tabint(T) ;
+	printf("trié\n") ;
+	tri_bulle_tabint(T) ;
+	aff_tabint(T) ;
+	//ecrire_dans_test_tri_bulle() ;
+	
+	printf("nombre de comparaisons de cases : %lld\n",NBCOMP) ;
+	printf("nombre d'echange de contenu : %lld\n",NBECH) ;
+	
+	
+	int i;
+	FILE *F ;
+	struct stat stats ;
+	F = fopen("test_tri_bulle.data","w") ;
+	for(i=10;i<1000;i=i*1.2) {
+		printf("%d\n",i) ;
+		stats = stat_moy(i,1000) ;
+		fprintf(F,"   %d           %2f           %2f\n",stats.N,stats.nb_moy_comp,stats.nb_moy_ech) ;
+	}
+	fclose(F) ;
+	
+	// Le code ci-dessous est provisoire, juste pour faire marche l'enchainement du Makefile
+	// Fin du code provisoire
+	exit(0);
+}
